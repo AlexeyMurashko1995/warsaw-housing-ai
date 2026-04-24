ef1a34246ef2d06e52d5798cf1b7f1375cc9e104
AlexMurashko <alexeymurashko1995@gmail.com>
2026-04-24 22:39:27 +0200

    feat: add BeautifulSoup scraper with price cleaning and outlier filtering (>100k)

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 target_url = 'https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie/warszawa'
@@ -8,3 +9,20 @@ headers = {
 
 response = requests.get(target_url, headers=headers, timeout=10)
 print(f'Response status code: {response.status_code}')
+
+soup = BeautifulSoup(response.text, 'html.parser')
+prices_all = soup.find_all('span', class_='css-6t3bie')
+
+prices_list = []
+
+for price in prices_all:
+    if 'Zapytaj o cenę' in price.text:
+        continue
+    else:
+        current_price = price.text
+        clean_price = current_price.replace('\xa0', '').replace('zł', '')
+        final_price = int(clean_price)
+        if final_price > 100000:
+            prices_list.append(final_price)
+
+print(f'Average price: {sum(prices_list)/len(prices_list):.0f} zł')
